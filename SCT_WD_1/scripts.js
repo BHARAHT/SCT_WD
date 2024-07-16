f63a71811c5866c42f0b8372aaca5425394bad44
BHARAHT <70618787+BHARAHT@users.noreply.github.com>
2024-07-16 23:38:40 +0530

    Add files via upload

---
+++ SCT_WD_1/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+        navbar.style.padding = '10px 0';
+        navbar.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.3)';
+    } else {
+        navbar.style.backgroundColor = '#333';
+        navbar.style.padding = '15px 0';
+        navbar.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.2)';
+    }
+});
